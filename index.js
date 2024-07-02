5d68821476019aaf491695e86a7efec15ab6ac3f
Riday1 <rrriday100@gmail.com>
2024-07-02 12:19:41 +0600

    env file is added

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-
-
-
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = "mongodb+srv://carPractice:<password>@cluster0.w8wev2h.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+
+        const cakesCollection = client.db('TastyTreat').collection('cakes')
+        const ordersCollection = client.db('TastyTreat').collection('orders')
+
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
@@ -19,4 +48,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`)
 })
+
+// TastyTreat:Brw1mWUIsuQCmDTs
 
